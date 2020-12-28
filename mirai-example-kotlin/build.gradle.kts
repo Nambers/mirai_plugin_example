0865220d9141105a6a108ffb3ed0a9a59622e2e2
Bryan Chiu <1930893235@qq.com>
2020-12-28 21:22:23 +0800

    更新2.0-M2版本

--- mirai-example-kotlin/build.gradle.kts
+++ mirai-example-kotlin/build.gradle.kts
@@ -1,17 +1,17 @@
 plugins {
-    val kotlinVersion = "1.4.10"
+    val kotlinVersion = "1.4.21"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
-    id("net.mamoe.mirai-console") version "2.0-M1-1"
+    id("net.mamoe.mirai-console") version "2.0-M2"
 }
 
 group = "org.example"
-version = "0.2.0"
+version = "0.1.0"
 
 repositories {
-        //国内镜像源
-    maven { url =uri("https://maven.aliyun.com/nexus/content/repositories/jcenter")}
+    //国内镜像源
     maven { url =uri("https://mirrors.huaweicloud.com/repository/maven") }
+    maven { url =uri("https://maven.aliyun.com/nexus/content/repositories/jcenter")}
     maven { url =uri("https://dl.bintray.com/kotlin/kotlin-eap")}
     mavenLocal()
     mavenCentral()
--- mirai-example-kotlin/src/main/kotlin/PluginMain.kt
+++ mirai-example-kotlin/src/main/kotlin/PluginMain.kt
@@ -2,14 +2,16 @@ package org.example.mirai.plugin
 
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
+import net.mamoe.mirai.event.Listener
 import net.mamoe.mirai.event.events.BotInvitedJoinGroupRequestEvent
 import net.mamoe.mirai.event.events.FriendMessageEvent
 import net.mamoe.mirai.event.events.GroupMessageEvent
 import net.mamoe.mirai.event.events.NewFriendRequestEvent
+import net.mamoe.mirai.event.globalEventChannel
 import net.mamoe.mirai.utils.info
-import net.mamoe.mirai.event.subscribeAlways
 import net.mamoe.mirai.message.data.Image
 import net.mamoe.mirai.message.data.PlainText
+import kotlin.coroutines.EmptyCoroutineContext
 
 /*
 在src/main/resources/plugin.yml里改插件信息和入口点
@@ -27,33 +29,48 @@ object PluginMain : KotlinPlugin(
         logger.info { "Plugin loaded" }
         //配置文件目录 "${dataFolder.absolutePath}/"
 
-        subscribeAlways<GroupMessageEvent> {
+        globalEventChannel().subscribeAlways(
+            GroupMessageEvent::class,
+            EmptyCoroutineContext,
+            Listener.ConcurrencyKind.CONCURRENT
+        ) {
             //群消息
-            if(message.contentToString().startsWith("复读")){
-                //reply 也可以换成 group.sendMessage()，reply会自动退出如果没记错，sendmessage不会
-                reply(message.contentToString().replace("复读", ""))
+            if (message.contentToString().startsWith("复读")) {
+                group.sendMessage(message.contentToString().replace("复读", ""))
             }
-            if(message.contentToString() == "hi"){
-                reply("hi")
+            if (message.contentToString() == "hi") {
+                group.sendMessage("hi")
             }
             message.forEach {
                 //循环每个元素在消息里
-                if(it is Image){
+                if (it is Image) {
                     //如果消息这一部分是图片
                 }
-                if(it is PlainText){
+                if (it is PlainText) {
                     //如果消息这一部分是纯文本
                 }
             }
         }
-        subscribeAlways<FriendMessageEvent>{
+        globalEventChannel().subscribeAlways(
+            FriendMessageEvent::class,
+            EmptyCoroutineContext,
+            Listener.ConcurrencyKind.CONCURRENT
+        ) {
             //好友信息
         }
-        subscribeAlways<NewFriendRequestEvent> {
+        globalEventChannel().subscribeAlways(
+            NewFriendRequestEvent::class,
+            EmptyCoroutineContext,
+            Listener.ConcurrencyKind.CONCURRENT
+        ) {
             //自动同意好友申请
             accept()
         }
-        subscribeAlways<BotInvitedJoinGroupRequestEvent> {
+        globalEventChannel().subscribeAlways(
+            BotInvitedJoinGroupRequestEvent::class,
+            EmptyCoroutineContext,
+            Listener.ConcurrencyKind.CONCURRENT
+        ) {
             //自动同意加群申请
             accept()
         }
