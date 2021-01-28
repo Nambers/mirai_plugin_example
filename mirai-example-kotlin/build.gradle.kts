d3368ab7a5057c8173a467885cb29b602b9c1e15
Eritque arcus <1930893235@qq.com>
2021-01-28 23:47:20 +0800

    优化

--- mirai-example-kotlin/build.gradle.kts
+++ mirai-example-kotlin/build.gradle.kts
@@ -5,6 +5,12 @@ plugins {
     id("net.mamoe.mirai-console") version "2.0.0"
 }
 
+mirai { // this: MiraiConsoleExtension
+    // 可指定mirai-core配置，
+    //参考 https://github.com/mamoe/mirai-console/blob/master/tools/gradle-plugin/README.md#%E9%85%8D%E7%BD%AE
+    coreVersion = "2.1.0" // 修改 mirai-core 版本
+}
+
 group = "org.example"
 version = "0.1.0"
 
@@ -21,5 +27,5 @@ repositories {
 dependencies{
     //在IDE内运行的mcl添加滑块模块，参考https://github.com/project-mirai/mirai-login-solver-selenium
     //runtimeOnly("net.mamoe:mirai-login-solver-selenium:1.0-dev-15")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.4.21")
+    //implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.4.21")
 }
